4b89b3a17e462318dd1c60075d730c44340521b4
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2023-04-28 10:38:34 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,27 +1,38 @@
-def dfs(x,y):
-    if graph[x][y] == '-':
-        graph[x][y] = 1	   
-        for _y in [1,-1]:   
-            Y = y + _y
-            if (Y > 0 and Y < m) and graph[x][Y] == '-':
-                dfs(x,Y)
-    if graph[x][y] == '|':
-        graph[x][y] = 1	    
-        for _x in [1,-1]: 
-            X = x + _x  
-            if (X > 0 and X < n) and graph[X][y] == '|':
-                dfs(X,y)
- 
-n,m = map(int, input().split()) 
-graph = [] 
-for _ in range(n):
-    graph.append(list(input()))
- 
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split()) #방 바닥의 세로 크기 / 방 바악의 가로 크기
+
+floor = []
+for i in range(N):
+    floor.append(list(map(str, input().rstrip())))
+# for j in range(N):
+#     print(floor[j])
+
 count = 0
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == '-' or graph[i][j] == '|':
-            dfs(i,j)   
-            count += 1
- 
-print(count)
+def dfsP(x, y):
+    ny = y + 1
+    floor[y][x] = '1'
+    if ny < N and floor[ny][x] == '|' :
+        dfsP(x, ny)
+
+def dfsH(x, y):
+    nx = x + 1
+    floor[y][x] = '1'
+    if nx < M and floor[y][nx] == '-':
+        dfsH(nx, y)
+
+def sol():
+    global count
+    for i in range(M):
+        for j in range(N):
+            if floor[j][i] == '-':
+                dfsH(i, j)
+                count += 1
+            elif floor[j][i] == '|':
+                dfsP(i, j)
+                count += 1
+    print(count)
+
+sol()
