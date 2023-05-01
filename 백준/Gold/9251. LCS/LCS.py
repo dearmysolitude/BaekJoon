34eb7dd80035c603f6469cbbcb3795c1a12a2c7b
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2023-05-01 17:22:44 +0900

    [Gold V] Title: LCS, Time: 532 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -5,8 +5,8 @@ fstr = input().rstrip()
 sstr = input().rstrip()
 ln1 = len(fstr)
 ln2 = len(sstr)
-
-table = [([0] * (ln2+1)) for _ in range(ln1+1)] #(fstr수+1) 개의 열, (sstr수+1)개의 행 생성
+#인덱스 오류: 아래 테이블 생성에서 ln2와 ln1을 바꾸었더니 실행됨
+table = [([0] * (ln2+1)) for _ in range(ln1+1)]
 #0인덱스는 모두 0으로 되어있고, 그 다음 인덱스, 1부터를 str의 몇 번째 글자인지 확인하는 것이다(인덱스 1인 경우 해당 str의 첫번재 글자).
 
 # table[i][j]를 완성하기 위해 table[i-1][j], table[i][j-1], table[i-1][j-1] 셋을 알아야 함.(점화식은 아래 for문을 통해 확인하자)
