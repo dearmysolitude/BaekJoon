fc99fced14dfb8f05a442aed84a3fdc43daff62b
Lucid <optimisticnihilism2007@gmail.com>
2023-05-23 20:06:55 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -4,11 +4,12 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 
 def TwobyN(n):
-    dp = [0 for i in range(1001)]
+    dp = [0 for i in range(n+2)]
     dp[1] = 1
     dp[2] = 2
-    for i in range(3, 1001):
+    for i in range(3, n+2):
         dp[i] = (dp[i-2] + dp[i-1]) % 10007
     print(dp[n])
+    return
 
 TwobyN(N)
