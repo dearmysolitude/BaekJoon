737126d151eedba9501123205d2b5935f1c9bcfc
Lucid <optimisticnihilism2007@gmail.com>
2023-05-29 14:09:37 +0900

    [Silver V] Title: 집합, Time: 3692 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -6,6 +6,7 @@ N = int(input().rstrip())
 # 어떤 자료구조 써야하나? 링크드 리스트?
 # 카운트 소트?
 # 리스트의 인덱스를 특정 값으로 두고 그 인덱스의 값을 있는지1 없는지0 판단하는 데 써보자
+# 출제 의도 -> 비트마스킹
 
 NUMBERS = 21
 nums = [0] * NUMBERS
@@ -19,26 +20,25 @@ def operate(n, array):
         order = operation[0]
         if order != 'all' and order != 'empty': 
             x = int(operation[1])
-
         if order == 'add':
-            array[x] = 1
+            array[x] = EXIST
         elif order == 'remove':
-            array[x] = 0
+            array[x] = EMPTY
         elif order == 'check':
-            if array[x] == 1:
+            if array[x] == EXIST:
                 print(EXIST)
             else:
                 print(EMPTY)
         elif order == 'toggle':
-            if array[x] == 1:
-                array[x] = 0
+            if array[x] == EXIST:
+                array[x] = EMPTY
             else:
-                array[x] = 1
+                array[x] = EXIST
         elif order == 'all':
             for i in range(1, NUMBERS):
-                array[i] = 1
+                array[i] = EXIST
         else:
             for i in range(1, NUMBERS):
-                array[i] = 0
+                array[i] = EMPTY
             
 operate(N, nums)
