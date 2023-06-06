157ae28bd148882d3ae9b39ae94615c083f34345
Lucid <optimisticnihilism2007@gmail.com>
2023-06-06 16:18:40 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 548 ms, Memory: 38120 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,39 +1,53 @@
+import sys
 from collections import deque
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)] # 그래프 생성
-visited = [[-1] * M for _ in range(N)] # 방문기록
- 
- # 방향탐색 범위설정
-dx, dy = [0,0,-1,1], [-1,1,0,0]
+input = sys.stdin.readline
 
-def bfs(i,j):
-    queue = deque()
-    queue.append((i,j))
-
-    visited[i][j] = 0
-
-    while queue:
-        x, y = queue.popleft()
-        
+# 목표 지점부터 시작한다.
+def navi(arr, y, x, n, m): # 목표지점의 좌표를 넣어..
+    q = deque()
+    q.append([y, x])
+    arr[y][x] = 0
+    desX = x
+    desY = y
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    while q:
+        y, x = map(int, q.popleft())
         for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (0<= nx < m and 0 <= ny < n and arr[ny][nx] == 1):
+                if arr[ny][nx] == 0:
+                    continue
+                q.append([ny, nx])
+                arr[ny][nx] += arr[y][x]
+
+    for i in range(n): # 못가는 데는 -1로
+        for j in range(m):
+            if (arr[i][j] == 1):
+                arr[i][j] = -1
+
+    for i in range(4): # 2근처를 1로 만들기 위함
+        x = desX + dx[i]
+        y = desY + dy[i]
+        if 0 <= x < m and 0 <= y < n and arr[y][x] == 3: # 중복할 경우 최종 목적지 근처 값은 3이 된다.
+            arr[y][x] = 1
+
+n, m = map(int, input().split())  # n은 세로의 크기, m은 가로의 크기
+coor = [None] * n
+for i in range(n):
+    coor[i] = list(map(int, input().split()))
+
+# 목표점을 찾는다
+for i in range(n):
+    for j in range(m):
+        if coor[i][j] == 2:
+            destX = j
+            destY = i
             
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == -1:
-                if graph[nx][ny] == 0: # 갈 수 없는 땅 체크
-                    visited[nx][ny] = 0
-                elif graph[nx][ny] == 1: # 갈 수 있는 땅 체크
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append((nx,ny))
+navi(coor, destY, destX, n, m)
 
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 2 and visited[i][j] == -1:
-            bfs(i, j)
-
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 0:
-            print(0, end=' ')
-        else:
-            print(visited[i][j], end=' ')
+for i in range(n):
+    for j in range(m):
+        print(coor[i][j], end=' ')
     print()
