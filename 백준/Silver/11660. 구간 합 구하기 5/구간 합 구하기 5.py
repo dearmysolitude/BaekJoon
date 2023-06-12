ff0a51f3d4e630fe98dbceefeb65e34609ec54bf
Lucid <optimisticnihilism2007@gmail.com>
2023-06-13 00:20:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1076 ms, Memory: 106004 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -6,8 +6,8 @@ N, M = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 
 dp = [[0]*(N+1) for _ in range(N+1)] #구간합
-for i in range(1, N+1): # y
-    for j in range(1, N+1): # x
+for i in range(1, N+1):
+    for j in range(1, N+1):
         dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1]
 
 for i in range(M):
