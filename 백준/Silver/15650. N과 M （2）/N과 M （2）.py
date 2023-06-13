7e0f0b061bf981d1ea6ad80d638f8d04cab94ed1
Lucid <optimisticnihilism2007@gmail.com>
2023-06-13 10:30:32 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,19 @@
+import sys
+import itertools
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr = []
+for i in range(1, N+1):
+    arr.append(i)
+
+nCr = itertools.combinations(arr, M)
+a = list(nCr)
+
+for i in range(len(a)):
+    b = list(a[i])
+    b.sort()
+    for j in range(len(b)):
+        print(b[j], end = ' ')
+    print()
